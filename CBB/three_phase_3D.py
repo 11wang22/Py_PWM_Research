c45971f01d3d88ef1a9aedd3ce8bde0bd8f03700
11wang22 <38503539+11wang22@users.noreply.github.com>
2026-04-12 10:07:09 +0800

    finish 3D plot

--- CBB/three_phase_3D.py
+++ CBB/three_phase_3D.py
@@ -4,8 +4,8 @@ from matplotlib import cm
 
 class Threephase3D:
     def __init__(self):
-        self.wt = np.arange(0, 2 * np.pi / 3, 1e-3)
-        self.Modu = np.arange(0, 2 / np.sqrt(3), 1e-3)
+        self.wt = np.arange(0, 2 * np.pi / 3, 2e-3)
+        self.Modu = np.arange(0, 2 / np.sqrt(3), 2e-3)
         self.X = np.zeros((len(self.Modu), len(self.wt)))
         self.Y = np.zeros((len(self.Modu), len(self.wt)))
 
@@ -68,34 +68,57 @@ class Threephase3D:
         plt.rcParams['mathtext.fontset'] = 'stix'  # 让数学符号也匹配 Times 风格
         
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        plt.subplots_adjust(left=0.05, right=0.9, top=0.95, bottom=0.05)  # 去白边
+        plt.subplots_adjust(left=0.02, right=0.98, top=0.98, bottom=0.02)  # cut some white space around the plot
         
-        # ===================== 统一配色 =====================
-        cmap = cm.Blues  # 你可以换成 cm.plasma / cm.inferno / cm.Blues
+        # ===================== axis settings =====================
+        ##### set the projection type to orthographic for a more technical look
+        ax.set_proj_type('ortho')  
+        
+        ##### set facecolor to white for a cleaner background
+        ax.set_facecolor('white')
+        # close the panes to make the plot cleaner
+        ax.xaxis.pane.fill = False         
+        ax.yaxis.pane.fill = False
+        ax.zaxis.pane.fill = False
+        # set grid lines to a light gray for better visibility without overpowering the data
+        grid_color = "#C0C0C0"
+        ax.xaxis._axinfo['grid']['color'] = grid_color
+        ax.yaxis._axinfo['grid']['color'] = grid_color
+        ax.zaxis._axinfo['grid']['color'] = grid_color
+        # set the axis lines to a darker gray for better contrast
+        ax.xaxis.line.set_color('#666666')
+        ax.yaxis.line.set_color('#666666')
+        ax.zaxis.line.set_color('#666666')
+        
+        ##### set view angle for better visibility of the 3D structure
+        ax.view_init(elev=20, azim=-45, roll=0)
+        
+        # ===================== colorbar settigns =====================
+        cmap = cm.coolwarm  # 你可以换成 cm.plasma / cm.inferno / cm.Blues
         combined_data = np.concatenate([self.vzmax.ravel(), self.vzmin.ravel()])
         vmin, vmax = combined_data.min(), combined_data.max()
-
-        # ===================== 画两个曲面 =====================
-        surf1 = ax.plot_surface(
-            self.X, self.Y, self.vzmax,
-            cmap=cmap, alpha=0.9, vmin=vmin, vmax=vmax
-        )
+        
+        # ===================== plot two surfaces =====================
         surf2 = ax.plot_surface(
             self.X, self.Y, self.vzmin,
-            cmap=cmap, alpha=0.9, vmin=vmin, vmax=vmax
+            cmap=cmap, alpha=0.6, vmin=vmin, vmax=vmax,
+            linewidth=0.2, edgecolor='k', rstride=20, cstride=20  # add grid lines for better visibility
         )
-
-        # ===================== colorbar =====================
+                
+        surf1 = ax.plot_surface(
+            self.X, self.Y, self.vzmax,
+            cmap=cmap, alpha=0.6, vmin=vmin, vmax=vmax,
+            linewidth=0.2, edgecolor='k', rstride=20, cstride=20  # add grid lines for better visibility
+        )
         cbar = fig.colorbar(surf1, ax=ax, shrink=0.5, pad=0.1)
-        cbar.set_label('Zero-sequence Voltage (p.u.)', fontweight='bold')
 
-        # ===================== 标签字体 =====================
+        # ===================== label fonts =====================
         ax.set_xlabel('Angle (rad)', fontweight='bold')
         ax.set_ylabel('Modulation Index', fontweight='bold')
         ax.set_zlabel('Zero-sequence Voltage (p.u.)', fontweight='bold')
-        ax.set_title('Zero-sequence Voltage Limitation in Three-Phase Systems', fontweight='bold')
-        
-        # ===================== X轴刻度 =====================
+        ax.set_box_aspect(None, zoom=0.95)
+
+        # ===================== X axis label =====================
         xticks = [0, np.pi/6, np.pi/3, np.pi/2, 2*np.pi/3]
         xtick_labels = [r'$0$', r'$\pi/6$', r'$\pi/3$', r'$\pi/2$',r'$2\pi/3$']
         ax.set_xticks(xticks)
